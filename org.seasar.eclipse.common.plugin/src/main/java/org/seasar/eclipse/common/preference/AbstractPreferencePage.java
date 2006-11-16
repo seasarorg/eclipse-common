84dbe947f9b916c00cec4118bc1adfdcc5091a52
taichi <ryushi@gmail.com>
2006-11-16 13:45:47 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/eclipsecommon/trunk@4 7e3b1c58-a2f9-4d7c-b89e-add245db2cc1

---
+++ org.seasar.eclipse.common.plugin/src/main/java/org/seasar/eclipse/common/preference/AbstractPreferencePage.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.seasar.eclipse.common.preference;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+/**
+ * @author taichi
+ * 
+ */
+public abstract class AbstractPreferencePage extends PropertyPage implements
+		IWorkbenchPreferencePage {
+
+	protected IWorkbench workbench;
+
+	public AbstractPreferencePage() {
+		super();
+	}
+
+	public void init(IWorkbench workbench) {
+		this.workbench = workbench;
+	}
+
+	public boolean isProjectPage() {
+		return getProject() != null;
+	}
+
+	public IProject getProject() {
+		IProject result = null;
+		Object o = getElement();
+		if (o instanceof IProject) {
+			result = (IProject) o;
+		} else if (o instanceof IAdaptable) {
+			IAdaptable a = (IAdaptable) o;
+			result = (IProject) a.getAdapter(IProject.class);
+		}
+		return result;
+	}
+}
---
+++ org.seasar.eclipse.common.plugin/src/main/java/org/seasar/eclipse/common/util/PreferenceStoreUtil.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.seasar.eclipse.common.util;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ProjectScope;
+import org.eclipse.core.runtime.preferences.IScopeContext;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+
+/**
+ * @author taichi
+ * 
+ */
+public class PreferenceStoreUtil {
+	public static ScopedPreferenceStore getCascadingStore(String qualifier,
+			IProject project) {
+		IScopeContext[] ctxs = null;
+		if (project != null) {
+			ctxs = new IScopeContext[2];
+			ctxs[0] = new ProjectScope(project);
+			ctxs[1] = new InstanceScope();
+		} else {
+			ctxs = new IScopeContext[1];
+			ctxs[0] = new InstanceScope();
+		}
+
+		ScopedPreferenceStore store = new ScopedPreferenceStore(ctxs[0],
+				qualifier);
+		store.setSearchContexts(ctxs);
+		return store;
+	}
+}
