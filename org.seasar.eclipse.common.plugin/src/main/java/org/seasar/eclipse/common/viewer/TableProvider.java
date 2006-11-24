dcd53abd236780f93daf6d6176d1463fe22c27a9
y-komori <komo@littleforest.jp>
2006-11-24 13:13:29 +0000

    ・Tiger依存を修正.
    git-svn-id: https://www.seasar.org/svn/sandbox/eclipsecommon/trunk@11 7e3b1c58-a2f9-4d7c-b89e-add245db2cc1

--- org.seasar.eclipse.common.plugin/src/main/java/org/seasar/eclipse/common/viewer/TableProvider.java
+++ org.seasar.eclipse.common.plugin/src/main/java/org/seasar/eclipse/common/viewer/TableProvider.java
@@ -32,100 +32,100 @@ import org.seasar.framework.util.ArrayMap;
  * @see org.seasar.eclipse.common.viewer.ColumnDescriptor
  */
 public class TableProvider extends LabelProvider implements
-		ITableLabelProvider, ICellModifier {
+        ITableLabelProvider, ICellModifier {
 
-	private ArrayMap columnDescs;
+    private ArrayMap columnDescs;
 
-	private TableViewer viewer;
+    private TableViewer viewer;
 
-	public TableProvider(TableViewer viewer, ColumnDescriptor[] cds) {
-		this.viewer = viewer;
-		this.columnDescs = new ArrayMap(cds.length);
-		List<String> keys = new ArrayList<String>(cds.length);
-		List<CellEditor> editors = new ArrayList<CellEditor>(cds.length);
-		for (int i = 0; i < cds.length; i++) {
-			ColumnDescriptor cd = cds[i];
-			String name = cd.getName();
-			if (columnDescs.containsKey(name)) {
-				name = name + "@" + System.identityHashCode(cd);
-			}
-			this.columnDescs.put(name, cd);
-			keys.add(name);
-			editors.add(cd.getCellEditor());
-		}
-		this.viewer.setColumnProperties((String[]) keys.toArray(new String[keys
-				.size()]));
-		this.viewer.setCellEditors((CellEditor[]) editors
-				.toArray(new CellEditor[editors.size()]));
-		this.viewer.setCellModifier(this);
-	}
+    public TableProvider(TableViewer viewer, ColumnDescriptor[] cds) {
+        this.viewer = viewer;
+        this.columnDescs = new ArrayMap(cds.length);
+        List keys = new ArrayList(cds.length);
+        List editors = new ArrayList(cds.length);
+        for (int i = 0; i < cds.length; i++) {
+            ColumnDescriptor cd = cds[i];
+            String name = cd.getName();
+            if (columnDescs.containsKey(name)) {
+                name = name + "@" + System.identityHashCode(cd);
+            }
+            this.columnDescs.put(name, cd);
+            keys.add(name);
+            editors.add(cd.getCellEditor());
+        }
+        this.viewer.setColumnProperties((String[]) keys.toArray(new String[keys
+                .size()]));
+        this.viewer.setCellEditors((CellEditor[]) editors
+                .toArray(new CellEditor[editors.size()]));
+        this.viewer.setCellModifier(this);
+    }
 
-	protected ColumnDescriptor getDescriptor(String name) {
-		return (ColumnDescriptor) this.columnDescs.get(name);
-	}
+    protected ColumnDescriptor getDescriptor(String name) {
+        return (ColumnDescriptor) this.columnDescs.get(name);
+    }
 
-	protected ColumnDescriptor getDescriptor(int index) {
-		return (ColumnDescriptor) this.columnDescs.get(index);
-	}
+    protected ColumnDescriptor getDescriptor(int index) {
+        return (ColumnDescriptor) this.columnDescs.get(index);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnImage(java.lang.Object,
-	 *      int)
-	 */
-	public Image getColumnImage(Object element, int columnIndex) {
-		ColumnDescriptor cd = getDescriptor(columnIndex);
-		return cd != null ? cd.getImage(element) : null;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnImage(java.lang.Object,
+     *      int)
+     */
+    public Image getColumnImage(Object element, int columnIndex) {
+        ColumnDescriptor cd = getDescriptor(columnIndex);
+        return cd != null ? cd.getImage(element) : null;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnText(java.lang.Object,
-	 *      int)
-	 */
-	public String getColumnText(Object element, int columnIndex) {
-		ColumnDescriptor cd = getDescriptor(columnIndex);
-		return cd != null ? cd.getText(element) : "";
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnText(java.lang.Object,
+     *      int)
+     */
+    public String getColumnText(Object element, int columnIndex) {
+        ColumnDescriptor cd = getDescriptor(columnIndex);
+        return cd != null ? cd.getText(element) : "";
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.viewers.ICellModifier#canModify(java.lang.Object,
-	 *      java.lang.String)
-	 */
-	public boolean canModify(Object element, String property) {
-		ColumnDescriptor cd = getDescriptor(property);
-		return cd != null ? cd.canModify() : false;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.viewers.ICellModifier#canModify(java.lang.Object,
+     *      java.lang.String)
+     */
+    public boolean canModify(Object element, String property) {
+        ColumnDescriptor cd = getDescriptor(property);
+        return cd != null ? cd.canModify() : false;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.viewers.ICellModifier#getValue(java.lang.Object,
-	 *      java.lang.String)
-	 */
-	public Object getValue(Object element, String property) {
-		ColumnDescriptor cd = getDescriptor(property);
-		return cd != null ? cd.getValue(element) : null;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.viewers.ICellModifier#getValue(java.lang.Object,
+     *      java.lang.String)
+     */
+    public Object getValue(Object element, String property) {
+        ColumnDescriptor cd = getDescriptor(property);
+        return cd != null ? cd.getValue(element) : null;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.viewers.ICellModifier#modify(java.lang.Object,
-	 *      java.lang.String, java.lang.Object)
-	 */
-	public void modify(Object element, String property, Object value) {
-		if (element instanceof Item) {
-			element = ((Item) element).getData();
-		}
-		ColumnDescriptor cd = getDescriptor(property);
-		if (cd != null) {
-			cd.setValue(element, value);
-			this.viewer.update(element, new String[] { property });
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.viewers.ICellModifier#modify(java.lang.Object,
+     *      java.lang.String, java.lang.Object)
+     */
+    public void modify(Object element, String property, Object value) {
+        if (element instanceof Item) {
+            element = ((Item) element).getData();
+        }
+        ColumnDescriptor cd = getDescriptor(property);
+        if (cd != null) {
+            cd.setValue(element, value);
+            this.viewer.update(element, new String[] { property });
+        }
+    }
 }
--- org.seasar.eclipse.common.plugin/src/main/java/org/seasar/eclipse/common/wiget/ResourceTreeSelectionDialog.java
+++ org.seasar.eclipse.common.plugin/src/main/java/org/seasar/eclipse/common/wiget/ResourceTreeSelectionDialog.java
@@ -39,136 +39,136 @@ import org.seasar.eclipse.common.util.LogUtil;
  */
 public class ResourceTreeSelectionDialog extends ElementTreeSelectionDialog {
 
-	/**
-	 * @param parent
-	 * @param labelProvider
-	 * @param contentProvider
-	 */
-	public ResourceTreeSelectionDialog(Shell parent, IContainer root, int flags) {
-		super(parent, new ResourceLabelProvider(),
-				new ResourceTreeContentProvider(flags));
-		setInput(root);
-	}
+    /**
+     * @param parent
+     * @param labelProvider
+     * @param contentProvider
+     */
+    public ResourceTreeSelectionDialog(Shell parent, IContainer root, int flags) {
+        super(parent, new ResourceLabelProvider(),
+                new ResourceTreeContentProvider(flags));
+        setInput(root);
+    }
 
-	private static class ResourceLabelProvider extends LabelProvider implements
-			ILabelProvider {
-		public Image getImage(Object element) {
-			Image result = null;
-			if (element instanceof IResource) {
-				IResource r = (IResource) element;
-				IWorkbenchAdapter adapter = (IWorkbenchAdapter) r
-						.getAdapter(IWorkbenchAdapter.class);
-				if (adapter != null) {
-					ImageDescriptor desc = adapter.getImageDescriptor(r);
-					if (desc != null) {
-						result = desc.createImage();
-					}
-				}
-			}
-			return result;
-		}
+    private static class ResourceLabelProvider extends LabelProvider implements
+            ILabelProvider {
+        public Image getImage(Object element) {
+            Image result = null;
+            if (element instanceof IResource) {
+                IResource r = (IResource) element;
+                IWorkbenchAdapter adapter = (IWorkbenchAdapter) r
+                        .getAdapter(IWorkbenchAdapter.class);
+                if (adapter != null) {
+                    ImageDescriptor desc = adapter.getImageDescriptor(r);
+                    if (desc != null) {
+                        result = desc.createImage();
+                    }
+                }
+            }
+            return result;
+        }
 
-		public String getText(Object element) {
-			if (element instanceof IResource) {
-				IResource r = (IResource) element;
-				return r.getName();
-			}
-			return super.getText(element);
-		}
-	}
+        public String getText(Object element) {
+            if (element instanceof IResource) {
+                IResource r = (IResource) element;
+                return r.getName();
+            }
+            return super.getText(element);
+        }
+    }
 
-	private static class ResourceTreeContentProvider implements
-			ITreeContentProvider {
-		private int flags = IResource.NONE;
+    private static class ResourceTreeContentProvider implements
+            ITreeContentProvider {
+        private int flags = IResource.NONE;
 
-		public ResourceTreeContentProvider(int flags) {
-			this.flags = flags;
-		}
+        public ResourceTreeContentProvider(int flags) {
+            this.flags = flags;
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
-		 */
-		public Object[] getChildren(Object parentElement) {
-			if (parentElement instanceof IContainer) {
-				IContainer c = (IContainer) parentElement;
-				return members(c);
-			}
-			return null;
-		}
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
+         */
+        public Object[] getChildren(Object parentElement) {
+            if (parentElement instanceof IContainer) {
+                IContainer c = (IContainer) parentElement;
+                return members(c);
+            }
+            return null;
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
-		 */
-		public Object getParent(Object element) {
-			if (element instanceof IResource) {
-				IResource r = (IResource) element;
-				return r.getParent();
-			}
-			return null;
-		}
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
+         */
+        public Object getParent(Object element) {
+            if (element instanceof IResource) {
+                IResource r = (IResource) element;
+                return r.getParent();
+            }
+            return null;
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
-		 */
-		public boolean hasChildren(Object element) {
-			if (element instanceof IResource) {
-				IResource r = (IResource) element;
-				return r.getType() == IResource.PROJECT
-						|| r.getType() == IResource.FOLDER;
-			}
-			return false;
-		}
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
+         */
+        public boolean hasChildren(Object element) {
+            if (element instanceof IResource) {
+                IResource r = (IResource) element;
+                return r.getType() == IResource.PROJECT
+                        || r.getType() == IResource.FOLDER;
+            }
+            return false;
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
-		 */
-		public void dispose() {
-		}
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+         */
+        public void dispose() {
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer,
-		 *      java.lang.Object, java.lang.Object)
-		 */
-		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		}
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer,
+         *      java.lang.Object, java.lang.Object)
+         */
+        public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
-		 */
-		public Object[] getElements(Object inputElement) {
-			if (inputElement instanceof IContainer) {
-				IContainer c = (IContainer) inputElement;
-				return members(c);
-			}
-			return null;
-		}
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+         */
+        public Object[] getElements(Object inputElement) {
+            if (inputElement instanceof IContainer) {
+                IContainer c = (IContainer) inputElement;
+                return members(c);
+            }
+            return null;
+        }
 
-		private Object[] members(IContainer c) {
-			final List<IResource> result = new ArrayList<IResource>();
-			try {
-				IResource[] members = c.members();
-				for (int i = 0; i < members.length; i++) {
-					IResource resource = members[i];
-					if ((resource.getType() & flags) != 0) {
-						result.add(resource);
-					}
-				}
-			} catch (CoreException e) {
-				LogUtil.log(ResourcesPlugin.getPlugin(), e);
-			}
-			return result.toArray(new IResource[result.size()]);
-		}
-	}
+        private Object[] members(IContainer c) {
+            final List result = new ArrayList();
+            try {
+                IResource[] members = c.members();
+                for (int i = 0; i < members.length; i++) {
+                    IResource resource = members[i];
+                    if ((resource.getType() & flags) != 0) {
+                        result.add(resource);
+                    }
+                }
+            } catch (CoreException e) {
+                LogUtil.log(ResourcesPlugin.getPlugin(), e);
+            }
+            return result.toArray(new IResource[result.size()]);
+        }
+    }
 }
